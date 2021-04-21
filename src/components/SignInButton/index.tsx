7d31464dd35dfa3dd1d3eacc5a78158797b8c62e
Vagner Reis <vagnereis10@gmail.com>
2021-04-21 13:39:27 -0300

    Header component and begining Home component

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,20 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss";
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button type="submit" className={styles.signInButton}>
+      <FaGithub color="#84d361" />
+      vagnereix
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="submit" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in with Github
+    </button>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import styles from "./home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            Nesw about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
+}
